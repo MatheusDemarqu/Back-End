ea3af56a0937051307f6fcaaa867cb4169305b56
MatheusDemarqu <matheus.castoldi.demarqui2@protonmail.com>
2026-03-05 11:44:07 -0300

    Update comment on variable type checking

--- conteudo/cont1.py
+++ conteudo/cont1.py
@@ -2,4 +2,4 @@
 #float = para número reais ou com virgula EX: 0.45, 7.6
 #Bool = para valores lógicos de verdadeiro ou falso (Sempre usar com letra maiúscula)
 #str = para strings que são palavras caracteres ou textos. Ex: 'ola'. '7.5'
-#paa saber
+#para saber o tipo de uma variável pode se usar o comando print(type(variavel))
