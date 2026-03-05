68dcc4f5da5b574702809269cc7e146e5b65caa1
MatheusDemarqu <matheus.castoldi.demarqui2@protonmail.com>
2026-03-05 11:36:39 -0300

    Add comments explaining data types in Python

---
+++ conteudo/cont1.py
@@ -0,0 +1,5 @@
+#int = números inteiros. Ex: 7, 0, -4
+#float = para número reais ou com virgula EX: 0.45, 7.6
+#Bool = para valores lógicos de verdadeiro ou falso (Sempre usar com letra maiúscula)
+#str = para strings que são palavras caracteres ou textos. Ex: 'ola'. '7.5'
+#paa saber
