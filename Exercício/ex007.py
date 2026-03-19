eeeb00ee4873cc8e05aa1893777a1d419804d4f6
MatheusDemarqu <matheus.castoldi.demarqui2@protonmail.com>
2026-03-19 08:02:52 -0300

    Add script to calculate double, triple, and square root

---
+++ Exercício/ex007.py
@@ -0,0 +1,3 @@
+n1 = int(input("Escolha um número"))
+print("O número digitado foi:{}, sendo o o dobro:{}, o triplo é:{}, e a raiz quadrada é:{:.2f}". format(n1, n1*2, n1*3, n1**0.5))
+#para calcular a raiz quadrada eu posso utilizar a função pow(n**0.5)
