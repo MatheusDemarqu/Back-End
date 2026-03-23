06c8a6dccddd03213d02f620bdb33a624091f793
MatheusDemarqu <matheus.castoldi.demarqui2@protonmail.com>
2026-03-23 10:58:05 -0300

    Create ex014.py

---
+++ Exercício/ex014.py
@@ -0,0 +1,3 @@
+salario = float(input("Qual o valor do salário"))
+aumento = salario + (salario * (15 / 100))
+print("Seu salario era de:{:.2f}, agora com o novo aumento é de:{:.2f}". format(salario, aumento))
