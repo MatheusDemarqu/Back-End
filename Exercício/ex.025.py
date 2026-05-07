7c974ed117954c5202e70c5389953d80ba9a35e2
MatheusDemarqu <matheus.castoldi.demarqui2@protonmail.com>
2026-05-07 08:40:01 -0300

    Update ex.025.py

--- Exercício/ex.025.py
+++ Exercício/ex.025.py
@@ -1,6 +1,5 @@
 #Exercício Python 025: Crie um programa que leia o nome de uma pessoa
 e diga se ela tem "SILVA" no nome.
 
-nome = str(input('Digite o seu nome: ')).strip()
-
-print('SILVA' in nome.upper())
+nome = str(input(" Digite qual é o seu nome?")).strip()
+print('seu nome tem silva? {}'.format('silva' in nome.lower()))
