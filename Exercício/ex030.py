d3ac59cb64f165d8295930e227a5753a1dafaa3c
MatheusDemarqu <matheus.castoldi.demarqui2@protonmail.com>
2026-05-13 07:55:45 -0300

    Update ex030.py

--- Exercício/ex030.py
+++ Exercício/ex030.py
@@ -1,5 +1,6 @@
-numero = int(input("Informe um número inteiro"))
+numero = int(input("Informe um número inteiro:"))
 if numero%2 == 0:
     print("seu número é par")
 else:
     print("seu número é impar")
+    
