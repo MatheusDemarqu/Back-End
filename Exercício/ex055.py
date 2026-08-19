e24a2eb426c0dc99f8e981f49ff4d821169f0ea7
MatheusDemarqu <matheus.castoldi.demarqui2@protonmail.com>
2026-08-19 08:06:51 -0300

    Create ex055.py

---
+++ Exercício/ex055.py
@@ -0,0 +1,6 @@
+sexo = str(input( 'Informe seu dados [m/f]: ' ) . strip
+( ) . lower())
+while sexo not in 'mf':
+  sexo = str(input( 'dados inconsistentes informe novamente ' ) . strip() . lower( ) )
+  sexo. Iower() [0]
+print(f'O seu sexo é {sexo}' )
